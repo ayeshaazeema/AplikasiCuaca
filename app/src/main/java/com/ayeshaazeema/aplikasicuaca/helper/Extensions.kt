c37c0ed920fab7939ae6bf64a320218a45cc5f9a
ayeshaazeema <ayesha.azeema@gmail.com>
2021-04-13 09:25:53 +0700

    Third Commit

--- app/src/main/java/com/ayeshaazeema/aplikasicuaca/helper/Extensions.kt
+++ app/src/main/java/com/ayeshaazeema/aplikasicuaca/helper/Extensions.kt
@@ -1,39 +1,54 @@
 package com.ayeshaazeema.aplikasicuaca.helper
 
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.databinding.BindingAdapter
 import java.text.SimpleDateFormat
 import java.util.*
 
-fun dateConverter(): String {
+fun DateConverter(): String {
     val date = Calendar.getInstance().time
     val converter = SimpleDateFormat("EEE, d MMM yyyy", Locale("in"))
     val convertedDate = converter.format(date)
     return convertedDate
 }
 
-fun dayConverter(time: Long): String {
+fun DayConverter(time: Long): String {
     val converter = SimpleDateFormat("EEE, d MMM, yyyy hh:mm a")
     val convertedDay = converter.format(Date(time * 1000))
     return convertedDay
 }
 
-fun timeConverter(time: Long): String {
+fun TimeConverter(time: Long): String {
     val converter = SimpleDateFormat("hh:mm a")
     val convertedTime = converter.format(Date(time * 1000))
     return convertedTime
 }
 
 @BindingAdapter("android:dayConverter")
-fun convertToDay(view: TextView, value: Long) {
+fun ConvertToDay(view: TextView, value: Long) {
     val converter = SimpleDateFormat("EEE, d MM yyyy hh:mmm a")
     val convertedDay = converter.format(Date(value * 1000))
     view.text = convertedDay
 }
 
 @BindingAdapter("android:converterInt")
-fun convertToInt(view: TextView, value: Double) {
+fun ConvertToInt(view: TextView, value: Double) {
     val valueInt = value.toInt()
     val valueString = valueInt.toString()
     view.text = valueString
+}
+
+fun SetLayoutBackgroundImage(constraintLayout: ConstraintLayout, url: String?) {
+    when (url) {
+        "01d", "02d", "03d", "04d", "09d", "11d", "13d", "50d" -> {
+            constraintLayout.setBackgroundResource(
+                constraintLayout.resources.getIdentifier(
+                    "ic_background_daylight",
+                    "drawable",
+                    constraintLayout.context.packageName
+                )
+            )
+        }
+    }
 }
